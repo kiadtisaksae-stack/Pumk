aa4b8c8c7b9e9939d6245736e7b60e49e025f79d
Lokith11 <142744909+Lokith11@users.noreply.github.com>
2026-01-25 22:02:35 +0700

    f
    f

--- Assets/ElevatorEnums.cs
+++
@@ -1,7 +0,0 @@
-﻿
-public enum ServiceRequestType
-{
-    DeliverLuggage,    // 1. ส่งกระเป๋า (ไปที่ ServiceRoom)
-    DeliverFood,       // 2. ส่งหมูกระทะ (ไปที่ ServiceRoom)
-    FindLostItem       // 3. ช่วยหาของ (ไปที่ตำแหน่งของ Item นั้นๆ)
-}
